703170044aa964178ad81b66fa7c0099f4617d64
Alexander <alexanderpastfast@gmail.com>
2023-08-12 13:16:59 +0300

    fix: fix code to improve app

--- src/api/api.js
+++ src/api/api.js
@@ -1,24 +1,39 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "/api/",
+const $host = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
+const $authHost = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+const authInterceptor = (config) => {
+  config.headers.authorization = `Bearer ${localStorage.getItem("token")}`;
+  return config;
+};
+$authHost.interceptors.request.use(authInterceptor);
+
 export const productsAPI = {
-  getItems(value) {
-    return instance
-      .get("all")
+  getFilteredItems(value) {
+    return $host
+      .get("/api/product")
       .then((response) => response.data)
       .then((data) => {
-        return data.filter((product) => {
+        console.log(data);
+        return data.rows.filter((product) => {
           return (
             value &&
             product &&
-            product &&
-            product.title &&
-            product.title.toLowerCase().includes(value.toLowerCase())
+            product.name &&
+            product.name.toLowerCase().includes(value.toLowerCase())
           );
         });
       });
   },
+  getAllItems() {
+    return $host.get("/api/product").then((response) => response.data);
+  },
 };
+
+export { $host, $authHost };
